7955907adba013b0111c090254499a4120e77caf
Daemonickanec <z1a@ya.ru>
2021-06-21 01:05:01 +0300

    two final

--- main.py
+++ main.py
@@ -6,33 +6,23 @@ url = 'https://my-calend.ru/names/russian'
 page = req.get(url)
 soup = BeautifulSoup(page.text, 'lxml')
 
-list = []
-z = list
+z = []
 
 for x in soup.find_all('a', href=True):
-    table = x.get_text()
-    td_list = x.select('td')
-    list.append(table)
+    y = x.get_text()
+    z.append(y)
 
-
-def reverse(lst):
-    return lst[::-1]
-
-print(*reverse(z), sep=' ')
-
-
-print('Выберите имя будущего ребенка (1 - муж, 2 - жен, 3 - случайное слово): ')
-
+print('Нажмите "1" чтобы узнать имена ваших будущих детей: ')
 b = str(input())
 
 if b == '1':
-    print(z[113:168])
-
-elif b == '2':
-    print(z[57:112])
-
-elif b == '3':
-    print(random.choice(z))
-
+    print('Мальчик: ' + random.choice(z[113:168])  + '\n' + 'Девочка: ' + random.choice(z[57:112]))
 else:
     print('Вы ошиблись')
+
+
+
+
+
+
+
